6e2f02667bb0980d17e7c43a4081a716a056263b
auroii <auromartsojr@gmail.com>
2020-06-09 22:08:44 -0300

    class update comments...

--- DoubleLETT.hpp
+++ DoubleLETT.hpp
@@ -4,9 +4,19 @@
 using std::vector;
 
 
+/*
+Class with implements a double level euler tour tree
+@height: height of nodes in original tree
+@euler: euler tour array, TODO: each element represents an edge instead nodes
+@first: each element is the first apparition index in DFS
+@seen: auxiliar array for DFS euler tour
+@level2: sqrt decomposition of euler tour array
+
+*/
+
 class DoubleLETT {
 private:
-    vector<int> height, euler, first;
+    vector<int> height, euler, first; 
     vector<bool> seen;
     vector<int> level2;
     void eulerTour(vector<vector<int>> &g, int node, int h = 0);
